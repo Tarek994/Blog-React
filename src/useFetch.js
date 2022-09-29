478209fd7d4b38a2f844957d3ca8a31b669e16a7
“tarek994” <ghali.tarek.123@hotmail.com>
2022-09-30 01:15:36 +0200

    create custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from "react";
+
+const useFetch = (url) =>{
+    
+    const [data, setData] = useState(null);
+    const [isloading, setIsloading] = useState(false);
+    const [errMsg, setErrMsg] = useState(null);
+  
+    useEffect(() =>{
+      setIsloading(true);
+      setErrMsg(null);
+      fetch("http://localhost:4000/posts")
+      .then((res)=>{
+        console.log(res)
+        if(!res.ok){
+          throw Error(res.statusText ? res.statusText : "Error")
+         
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsloading(false)
+        console.log(data);
+        setData(data);
+        
+      }).catch((err) => {
+        setIsloading(false);
+        setErrMsg(err.message)
+      })
+  },[])
+    
+}
+
+export default useFetch
