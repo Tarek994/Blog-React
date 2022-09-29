dffe24467ec52dd68603c5e27694085146fcec66
“tarek994” <ghali.tarek.123@hotmail.com>
2022-09-30 01:40:11 +0200

    import-router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
